4071e7f739333c43b94abf9c2164f3e23219d037
divandia <106718176+Cvandia@users.noreply.github.com>
2023-06-05 13:20:50 +0800

    删除`_matched_groups`改为`RegxGroup`
    优化代码的布局

--- nonebot_plugin_genshin_cos/__init__.py
+++ nonebot_plugin_genshin_cos/__init__.py
@@ -1,26 +1,27 @@
-from nonebot.adapters.onebot.v11 import MessageSegment,MessageEvent,Bot,Message,GroupMessageEvent
-from nonebot.plugin import on_regex,PluginMetadata
+from nonebot.adapters.onebot.v11 import MessageSegment, MessageEvent, Bot, Message, GroupMessageEvent
+from nonebot.plugin import on_regex, PluginMetadata
 from nonebot.permission import SUPERUSER
-from nonebot.params import Arg
+from nonebot.params import Arg, RegexGroup
 from nonebot.exception import ActionFailed
 from nonebot.typing import T_State
 from nonebot import get_driver
-from .utils import get_cos,WriteError,check_cd
+from .utils import get_cos, WriteError, check_cd
 from .config import Config
 from nonebot.log import logger
+from typing import Tuple, Any
 from re import I
 import re
 from pathlib import Path
 
 __plugin_meta__ = PluginMetadata(
-    name = "米游社cos",
-    description = "获取原神coser图片",
-    config = Config,
-    usage = "原神cos",
-    extra = {
+    name="米游社cos",
+    description="获取原神coser图片",
+    config=Config,
+    usage="原神cos",
+    extra={
         "unique_name": "genshin_cos",
         "example": "保存cos:保存cos图片至本地文件",
-        "usage":"暂无",
+        "usage": "暂无",
         "author": "divandia <106718176+Cvandia@users.noreply.github.com>",
         "version": "0.1.5",
     },
@@ -30,12 +31,13 @@ max = Config.parse_obj(get_driver().config.dict()).cos_max
 save_path = Config.parse_obj(get_driver().config.dict()).cos_path
 user_data = {}
 
-send_cos = on_regex(r"^(原神|米游社)+cos(\s)?([x|*|X]\d)?",block=False,priority=5,flags=I)
-download_cos = on_regex(r"^(下载cos)|(cos保存)$",block=False,permission=SUPERUSER,flags=I)
+send_cos = on_regex(
+    r"^(原神|米游社)+cos(\s)?([x|*|X]\d)?", block=False, priority=5, flags=I)
+download_cos = on_regex(r"^(下载cos)|(cos保存)$", block=False,permission=SUPERUSER, flags=I)
 
 
 @download_cos.handle()
-async def choose(state:T_State,bot:Bot,event:MessageEvent):
+async def choose(state: T_State, bot: Bot, event: MessageEvent):
     choose_msg = ["点击下面的链接，预览选择你不需要的图片序号"]
     state['imgs'] = []
     N = 1
@@ -44,28 +46,29 @@ async def choose(state:T_State,bot:Bot,event:MessageEvent):
         state['imgs'].append(img)
         N += 1
     try:
-        await send_forward_msg(bot,event,"米游社cos",bot.self_id,choose_msg)
+        await send_forward_msg(bot, event, "米游社cos", bot.self_id, choose_msg)
     except ActionFailed:
         await download_cos.finish("合并转发失败，请重试，如果多次失败，可能是账户风控了")
-  
-@download_cos.got("num",prompt="请发送你不需要的链接序号\n发送“无”为全部下载,多个序号用空格隔开")
-async def down(state:T_State,num:Message = Arg()):
+
+
+@download_cos.got("num", prompt="请发送你不需要的链接序号\n发送“无”为全部下载,多个序号用空格隔开")
+async def down(state: T_State, num: Message = Arg()):
     got_msg = str(num)
     if got_msg == "无":
         try:
             dwn = get_cos()
-            if not save_path: 
+            if not save_path:
                 await download_cos.send("正在获取数据，未设置指定路径，默认下载到data/genshin_cos")
             else:
                 await download_cos.send("正在下载cos图片至指定文件夹,请稍等……")
             num = dwn.save_img(save_path)
             await download_cos.finish(f"保存完毕，一共保存了{num}张图片")
         except WriteError as exc:
-            await download_cos.finish(f"<{exc}>")           
-    elif not re.findall(r"^\d",got_msg):
+            await download_cos.finish(f"<{exc}>")
+    elif not re.findall(r"^\d", got_msg):
         await download_cos.finish("请发送正确的数字")
     else:
-        num_list = re.findall(r"\d+",got_msg)
+        num_list = re.findall(r"\d+", got_msg)
         names = get_cos().get_img_name()
         for n in num_list:
             try:
@@ -74,44 +77,40 @@ async def down(state:T_State,num:Message = Arg()):
             except IndexError as exc:
                 await download_cos.finish(f"出错了:<{exc}>\n可能是链接失效或者名称无效")
         try:
-            num = get_cos().download_urls(state['imgs'],names,save_path)
+            num = get_cos().download_urls(state['imgs'], names, save_path)
             await download_cos.finish(f"成功保存{num}张图片")
         except WriteError as exc:
             await download_cos.finish(f"出错了:<{exc}>")
-    
-        
-    
-
 
 
 max = Config.parse_obj(get_driver().config.dict()).cos_max
 
+
 @send_cos.handle()
-async def handle(bot:Bot, event:MessageEvent, state:T_State):
+async def handle(bot: Bot, event: MessageEvent, args: Tuple[Any, ...] = RegexGroup()):
     global user_data
-    args = list(state['_matched_groups'])
+    args = list(args)
     img = get_cos()
-    out_cd,deletime,user_data = check_cd(event.user_id,user_data)
+    out_cd, deletime, user_data = check_cd(event.user_id, user_data)
     if out_cd:
         if not args[2]:
             await send_cos.send("获取图片中…请稍等")
-            if not img.randow_cos_img():
+            if not await img.randow_cos_img():
                 await send_cos.finish("未获取到图片")
             try:
-                await send_cos.send(MessageSegment.image(img.randow_cos_img()))
+                await send_cos.send(MessageSegment.image(await img.randow_cos_img()))
             except ActionFailed:
-                await send_cos.finish("账户风控了,发送不了图片",at_sender = True)
+                await send_cos.finish("账户风控了,发送不了图片", at_sender=True)
         else:
-            num = int(re.sub(r"[x|*|X]","",args[2]))
+            num = int(re.sub(r"[x|*|X]", "", args[2]))
             num = num if num <= max else max
             msg_list = ['找到最新的一些cos图如下:']
-            imgs = img.get_img_url()
-            for i in range(0,num):
-                msg_list.append(MessageSegment.image(imgs[i])) 
-            await send_forward_msg(bot,event,"米游社cos",bot.self_id,msg_list)
+            imgs = await img.get_img_url()
+            for i in range(0, num):
+                msg_list.append(MessageSegment.image(imgs[i]))
+            await send_forward_msg(bot, event, "米游社cos", bot.self_id, msg_list)
     else:
-        await send_cos.finish(f"cd冷却中，还剩{deletime}秒",at_sender = True)
-
+        await send_cos.finish(f"cd冷却中，还剩{deletime}秒", at_sender=True)
 
 
 async def send_forward_msg(
@@ -122,7 +121,7 @@ async def send_forward_msg(
     msgs: list,
 ) -> dict:
     """调用合并转发API
-    
+
         bot: Bot
         event: 消息事件类型
         name: 发送者昵称
@@ -133,7 +132,7 @@ async def send_forward_msg(
         return {"type": "node", "data": {"name": name, "uin": uin, "content": msg}}
 
     messages = [to_json(msg) for msg in msgs]
-    
+
     if isinstance(event, GroupMessageEvent):
         return await bot.call_api(
             "send_group_forward_msg", group_id=event.group_id, messages=messages
@@ -141,4 +140,4 @@ async def send_forward_msg(
     else:
         return await bot.call_api(
             "send_private_forward_msg", user_id=event.user_id, messages=messages
-        )       
+        )
